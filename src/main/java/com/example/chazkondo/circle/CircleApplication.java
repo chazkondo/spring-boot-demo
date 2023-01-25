b064cfd9ebee444886f0d4a8a911f552a094e2e2
Chaz Kondo <chuckeekondo@gmail.com>
2023-01-25 12:18:10 -1000

    latest

--- src/main/java/com/example/chazkondo/circle/CircleApplication.java
+++ src/main/java/com/example/chazkondo/circle/CircleApplication.java
@@ -5,14 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @SpringBootApplication
 public class CircleApplication {
 
-	@RequestMapping("/")
+	@RequestMapping("/api")
 	@ResponseBody
-	String home() {
-		return "Hello World!";
+	public Map<String, String> home() {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("color1", "#123456");
+		map.put("color2", "#ABCDEF");
+		return map;
 	}
 
 	public static void main(String[] args) {
